c69699b439b022a3fc92cf1dded835db90d0985c
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-07-10 21:14:31 +0200

    tests cleanup
    removed useless initial test,
    facotorized browser acquisition with beforeEach

--- __tests__/browser_basic.specs.ts
+++ __tests__/browser_basic.specs.ts
@@ -1,24 +1,28 @@
+import { Page } from 'puppeteer';
+
 import Browser from '../src/browser';
 import ScrapedVideoData from '../src/video';
 
 jest.setTimeout(60000);
 
+let browser: Browser;
+let page: Page;
+
+beforeEach(async () => {
+  browser = await Browser.launch();
+  page = await browser.newPage();
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
 describe('Basic browser tests', () => {
-  it('should be able to launch a browser', async () => {
-    const browser = await Browser.launch();
-    browser.close();
-  });
-
   it('should scrape a single video', async () => {
-    const browser = await Browser.launch();
-    const page = await browser.newPage();
-
     const video = await ScrapedVideoData.scrape(page, 'https://www.youtube.com/watch?v=a1zevmYu1v4');
     expect(video.channelURL).toBe('https://www.youtube.com/c/FrançoisMariedeJouvencel');
     expect(video.title).toBe('Drone over Quiet Lake in the Morning');
     expect(+video.rawLikeCount).toBeGreaterThanOrEqual(1);
     expect(video.description).toBe('Another Bebop2 footage from a while ago.');
-
-    browser.close();
   });
 });
