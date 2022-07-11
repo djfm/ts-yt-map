5d12620de259fabb7399f289580fed24da929fd6
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-07-11 19:54:27 +0200

    commented experiments with request interception

--- src/bin/test-browser.ts
+++ src/bin/test-browser.ts
@@ -6,6 +6,26 @@ const start = async (): Promise<Browser> => {
   cfg.chrome.headless = false;
   const browser = await Browser.launch(cfg.chrome);
 
+  /*
+  const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+
+  page.on('request', (request) => {
+    // eslint-disable-next-line no-console
+    console.log(request.method(), request.url(), request.headers());
+    if (request.method() === 'POST') {
+      // request.abort();
+      request.continue();
+    }
+  });
+
+  page.on('response', (response) => {
+    // eslint-disable-next-line no-console
+    console.log(response.status(), response.url(), response.headers());
+  });
+  */
+
   return browser;
 };
 
