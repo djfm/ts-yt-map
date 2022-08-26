8bbdc908d4ef0f9075a5a4d2cbc5d24cfe1e759f
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-08-26 21:42:26 +0200

    tests should be ran with ./test

--- __tests__/integration/server_tests.ts
+++ __tests__/integration/server_tests.ts
@@ -1,53 +1,54 @@
 import { loadServerConfig, ServerConfig, createLogger, LoggerInterface } from '../../src/lib';
-import { startServer, ServerHandle } from '../../src/lib/server';
 import { API } from '../../src/lib/api';
 import { Client } from '../../src/lib/client';
 
 const password = 'secret';
 let cfg: ServerConfig;
-let server: ServerHandle;
 let serverURL: string;
 let client: Client;
 let log: LoggerInterface;
+let api: API;
 
 jest.setTimeout(300000);
 
 beforeAll(async () => {
   cfg = await loadServerConfig(password);
-  serverURL = `http://localhost:${cfg.port}`;
+  serverURL = `http://${cfg.host}:${cfg.port}`;
   log = await createLogger();
-  const api = new API(log, serverURL, password);
+  api = new API(log, serverURL, password);
   client = new Client(log, api);
-  server = await startServer(cfg, log);
-  log.info('Removing all recommendations from server before tests...');
-  await server.pg.query('TRUNCATE TABLE recommendation');
-  await server.pg.query('DELETE FROM video');
-  await server.pg.query('DELETE FROM channel');
+  const ran = await api.forTestingClearDb();
+  ran.forEach((r) => {
+    log.info(`RAN: ${r}`);
+  });
 });
 
 afterAll(async () => {
-  if (server) {
-    await server.close();
-  }
-
   log.close();
 });
 
 describe('the server abasic behaviour', () => {
   it('should get a URL to crawl from the server', async () => {
-    const api = new API(
-      log, serverURL, password,
-    );
-
     const url = await api.getUrlToCrawl();
     expect(url.length).toBeGreaterThan(0);
   });
 
   it('should scrape one video and its recommendations', async () => {
-    server.testing.resetTimeSinceLastURLToCrawlSent();
+    await api.forTestingResetTiming();
 
     const resp = await client.scrapeOneVideoAndItsRecommendations();
     expect(resp.ok).toBe(true);
     expect(resp.count).toBe(10);
+  });
+
+  it('should get 10 different URLs to crawl', async () => {
+    const api = new API(
+      log, serverURL, password,
+    );
+
+    const urlPromises = Array(10).fill(0).map(() => api.getUrlToCrawl());
+    const urls = await Promise.all(urlPromises);
+    const set = new Set(urls);
+    expect(set.size).toBe(10);
   });
 });
