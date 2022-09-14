001add01b4822c07e94c5c3424981897f1b7a351
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-09-14 18:37:50 +0200

    minimal migration to introduce client notion

--- data/migrations/4_create_client.sql
+++ data/migrations/4_create_client.sql
@@ -1,4 +1,4 @@
-create table Client (
+create table client (
     id serial primary key,
     ip text NOT NULL,
     name TEXT NULL,
@@ -6,11 +6,11 @@ create table Client (
     updated_at timestamp not null default current_timestamp
 );
 
-create unique index idx_ip on Client (ip);
-create unique index idx_name on Client (name);
+create unique index idx_ip_name on Client (ip, name);
 
-insert into Client (ip, name) values ('192.168.0.254','fm@paris');
+insert into client (ip, name) values ('192.168.0.254','fm@paris');
 
-select id from Client where name = 'fm@paris' into @clientId;
-
-alter table recommendation add column client_id integer null references Client(id) on delete cascade;
+alter table video add column client_id integer not null default -1;
+update video set client_id = (select id from client where name = 'fm@paris') where client_id = -1;
+alter table video add constraint fk_client_id foreign key (client_id) references client (id) on delete cascade;
+create index video_client_id_idx on video (client_id);
