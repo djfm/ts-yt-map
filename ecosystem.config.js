212d8920e0e679a764cea497e56c680f66172480
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2022-09-16 10:13:44 +0200

    another attempt at watching

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'server',
       script: 'src/bin/server.ts',
-      watch: ['src', 'views'],
+      watch: ['./src', './views'],
       ignore_watch: ['node_modules', 'logs'],
+      watch_delay: 200,
     },
   ],
 };
